70163aa4e1b3b813b3a885b4ad95a903e3942645
Ase <42305156+IrishMorales@users.noreply.github.com>
2023-02-15 14:57:44 +0800

    Update to use pre-commit hooks (#2)

--- tracker/urls.py
+++ tracker/urls.py
@@ -19,7 +19,9 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
+    path(
+        "", login_required(TemplateView.as_view(template_name="home.html")), name="home"
+    ),
 ]
