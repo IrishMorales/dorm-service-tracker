c6c13fa4f6660fe0e964168e5e5a7f2701c2940c
Ase <irish.morales@obf.ateneo.edu>
2023-02-21 12:09:15 +0800

    Add white card page

--- tracker/settings.py
+++ tracker/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "homepage",
     "scholar_profile",
+    "white_card",
 ]
 
 MIDDLEWARE = [
--- tracker/urls.py
+++ tracker/urls.py
@@ -19,6 +19,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from homepage.views import home_view
 from scholar_profile.views import profile_view
+from white_card.views import white_card_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
     ),  # includes pages for login, logout, password changess
     path("", login_required(home_view.as_view()), name="home"),
     path("profile/", login_required(profile_view.as_view()), name="profile"),
+    path("white_card/", login_required(white_card_view.as_view()), name="white_card"),
 ]
