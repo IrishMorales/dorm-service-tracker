677a6ea524daf950f43579c6006c45429e2c994f
Rajo Christian Cadorna <rajo.cadorna@obf.ateneo.edu>
2023-02-27 01:26:20 +0800

    registered models into admin.py

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Admins, Assignment, Registration, Scholar, ServiceHourListing, User
+
+admin.site.register(Admins)
+admin.site.register(Assignment)
+admin.site.register(Registration)
+admin.site.register(Scholar)
+admin.site.register(ServiceHourListing)
+admin.site.register(User)
--- Scholar/admin.py
+++ Scholar/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Registration, Scholar, ServiceHourListing, User
+
+admin.site.register(Registration)
+admin.site.register(Scholar)
+admin.site.register(ServiceHourListing)
+admin.site.register(User)
