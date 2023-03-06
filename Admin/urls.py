8c58371179ffc42e75bc67ca3e95705d78ee5b57
TambukinErick <erickgtl2001@gmail.com>
2023-03-06 14:17:16 +0800

    Created the admin terminal white card and scholar profile page

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,5 +1,28 @@
 from django.urls import path
 
-from . import views
+from Admin.views import (
+    admin_view,
+    scholars_listview,
+    signups_hours_listview,
+    admin_scholar_white_card,
+    admin_scholar_list_profile,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path("scholars_list/", (scholars_listview.as_view()), name="scholars-listview"),
+    path(
+        "signup_hours/",
+        (signups_hours_listview.as_view()),
+        name="signup-hours-listview",
+    ),
+    path(
+        "scholars_list/white_card/<int:user_id>",
+        admin_scholar_white_card,
+        name="admin_scholar_white_chard",
+    ),
+    path(
+        "scholars_list/profile/<int:user_id>",
+        admin_scholar_list_profile,
+        name="admin_scholar_profile",
+    ),
+]
