16413d9f26fdfe18cc6282fef7d17a1bdc56ce1a
Rajo Christian Cadorna <rajo.cadorna@obf.ateneo.edu>
2023-03-22 00:47:57 +0800

    implemented batch service_hour_listing creation

--- Admin/forms.py
+++ Admin/forms.py
@@ -9,13 +9,14 @@ LOCATIONS =(
     )
 
 class AddSlot(forms.Form):
+    serv_hours_task = forms.CharField()
+    serv_hours_loc = forms.ChoiceField(choices = LOCATIONS)
+    serv_hours_slot_count = forms.IntegerField()
+    serv_hours_date_start = forms.DateField()
+    serv_hours_date_end = forms.DateField()
+    
     class Meta:
-        serv_hours_task = forms.CharField()
-        serv_hours_loc = forms.ChoiceField(choices = LOCATIONS)
-        serv_hours_slot_count = forms.IntegerField()
-        serv_hours_date_start = forms.DateField()
-        serv_hours_date_end = forms.DateField()
-        
+        model = ServiceHourListing
 
     def __init__(self, *args, **kwargs):
         super(AddSlot, self).__init__(*args, **kwargs)
